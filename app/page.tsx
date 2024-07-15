02bf63858a4b2a515a1bf2712d58d8ce989e4578
Radhika <119474205+nisharadhika@users.noreply.github.com>
2024-07-15 18:43:24 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import './globals.css';
+
+
+export default function Home() {
+  return (
+    <div className="flex h-screen max-h-screen">
+    <section className="remove-scrollbar container my-auto">
+      <div className="sub-cantainer max-w-[496px]">
+
+      </div>
+
+    </section>
+    </div>
+  )
+  
+}
